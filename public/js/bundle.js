bab374b27263d027d0ee5778016a26ced252d425
bhardwajrizul <bhardwajrizul9816@gamil.com>
2023-01-07 23:12:02 +0530

    localhost API call  bug fix

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8458,7 +8458,7 @@ var updateSettings = /*#__PURE__*/function () {
       while (1) switch (_context.prev = _context.next) {
         case 0:
           _context.prev = 0;
-          url = type === 'password' ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword' : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+          url = type === 'password' ? '/api/v1/users/updateMyPassword' : '/api/v1/users/updateMe';
           _context.next = 4;
           return (0, _axios.default)({
             method: 'PATCH',
@@ -8724,7 +8724,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53636" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54033" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
