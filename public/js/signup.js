76d125da07c8ccaf06357499109529fc4c144cd4
bhardwajrizul <bhardwajrizul9816@gamil.com>
2023-01-07 19:49:31 +0530

    Initial Commit

---
+++ public/js/signup.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+
+
+export const signup = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
+      }
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'signup successfull!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
+
---
+++ routes/viewRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
+
+const router = express.Router();
+
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+
+router.get('/tour/:slug', authController.protect, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/signup', authController.isLoggedIn, viewsController.getSignupForm);
+router.get('/me', authController.protect, viewsController.getAccount);
+
+router.get(
+  '/my-tours',
+  bookingController.createBookingCheckout,
+  authController.protect,
+  viewsController.getMyTours
+);
+
+router.post(
+  '/submit-user-data',
+  authController.protect,
+  viewsController.updateUserData
+);
+
+module.exports = router;
